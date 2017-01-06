d252ce1da6c1676a4c992b3981e3a8416e40796e
gtalin <gtalin@users.noreply.github.com>
2017-01-06 22:37:23 +0000

    D3 bar chart

--- js/barchart.js
+++ js/barchart.js
@@ -4,49 +4,114 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
   console.log(gdp, year);
   drawGraph(gdp,year);
 });
-    	function drawGraph(gdp,year){
-    		var w = 900;
-			var h = 700;
-			var barPadding = 0;
-      var tooltip = d3.select("body")
-    .append("div")
-    .style("position", "absolute")
-    .style("z-index", "10")
-    .style("visibility", "hidden")
-    .style("background", "#fff")
-    .text("a simple tooltip");
 
-			svg = d3.select("body")
-					.append("svg")
-					.attr("width",w)
-					.attr("height",h);
-			var rect = svg.selectAll("rect")
-			   .data(gdp)
-			   .enter()
-			   .append("rect")
-         .attr("class", "tile")
-			   .attr("x", function(d, i) {
-			   		return i * (w / gdp.length);
-			   })
-			   .attr("y", function(d) {
-			   		return h - (d/40);
-			   })
-			   .attr("width", w / gdp.length - barPadding)
-			   .attr("height", function(d) {
-			   		return d/40;
-			   })
-               .attr("fill", "teal");
-            var label = svg.selectAll("text")
-            			   .data(gdp)
-            			   .enter()
-            			   .append("text");
-               /*rect.on("mouseover", function(d) { 
-               	console.log(d);
-  d3.select(this).select("text").style("visibility", "visible") 
+function extractYearRange(year) {
+  var year1 = year[0].split("-")[0];
+  var year2 = year[year.length-1].split("-")[0];
+  console.log(year1,year2);
+  console.log(Math.min( ...year));
+  return [year1,year2];
+}
+function extractGdpRange(gdp) {
+  var gdpMin = Math.min.apply(Math,gdp);
+  var gdpMax = Math.max( ...gdp);
+  console.log(gdpMin, gdpMax);
+  return [gdpMin, gdpMax];
+}
+
+function drawGraph(gdp,year){
+var w = 850;
+var h = 600;
+var barPadding = 0;
+var padding = 30;
+var yearRange = extractYearRange(year);
+var gdpRange = extractGdpRange(gdp);
+
+var tooltip = d3.select("body")
+.append("div")
+.style("position", "absolute")
+.style("z-index", "10")
+.style("visibility", "hidden")
+.style("background", "#fff")
+.text("a simple tooltip");
+
+var margin = {top: 80, right: 30, bottom: 80, left: 50};
+w = w - margin.right - margin.left;
+h = h - margin.top - margin.bottom;
+
+svg = d3.select("#container")
+		.append("svg")
+		.attr("width",w + margin.left + margin.right)
+		.attr("height",h + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top +")");
+/*svg is as per old w and h. For rest, w and h has changed 
+so they all appear with a nice margin on all sides*/
+var rect = svg.selectAll("rect")
+   .data(gdp)
+   .enter()
+   .append("rect")
+   .attr("class", "tile")
+   .attr("x", function(d, i) {
+   		return i * w / gdp.length;
+   })
+   .attr("y", function(d) {
+   		return h - d/45;
+   })
+   .attr("width", w / gdp.length - barPadding)
+   .attr("height", function(d) {
+   		return d/45;
+   })
+         .attr("fill", "teal");
+      var label = svg.selectAll("text")
+      			   .data(gdp)
+      			   .enter()
+      			   .append("text");
+         /*rect.on("mouseover", function(d) { 
+         	console.log(d);
+d3.select(this).select("text").style("visibility", "visible") 
 });*/
-rect.on("mouseover", function(d){tooltip.text(d); return tooltip.style("visibility", "visible");})
-      .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
-      .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+rect.on("mouseover", function(d){tooltip.text(d); 
+  return tooltip.style("visibility", "visible");})
+      .on("mousemove", function(){
+        return tooltip.style("top", (d3.event.pageY-10)+"px")
+               .style("left",(d3.event.pageX+10)+"px");})
+      .on("mouseout", function(){
+        return tooltip.style("visibility", "hidden");});
+
+
+
+    var widthScale = d3.scale.linear()
+                       .domain(yearRange)
+                      .range([0, w]);
+            /*coz shifted bar chart from 0 to (i+10) * (w-padding) / gdp.length;*/
+      var xAxis = d3.svg.axis()
+          .scale(widthScale)
+          .orient("bottom").ticks(10);
+
+
+    var heightScale = d3.scale.linear()
+        .domain([0,gdpRange[1]])
+        .range([h,0]);
+
+    var yAxis = d3.svg.axis()
+                  .scale(heightScale)
+                  .orient("left")
+                  /*.ticks(5);*/
+
+    svg.append("g")
+       .attr("class","axis")
+       .attr("transform", "translate(0," + (h) + ")")
+       .call(xAxis);
+    
+    svg.append("g")
+    .attr("class", "axis")
+    .call(yAxis);
+    /*.attr("transform", "translate(" + padding+ ",0)")
+    .call(yAxis);*/
+
+    /*coz shifted bar chart from (i+10) * (w-padding) / gdp.length;*/
+    /*d3.svg in v3 not v4*/
 /*var labels = rect.append("text")
   .text(function(d) { return d.name; })
   .style("visibility", "hidden");
